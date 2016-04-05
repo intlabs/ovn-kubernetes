2731eaaeae04796887940dcea21b067e067a8afb
Salvatore <salv.orlando@gmail.com>
2016-04-06 00:11:38 +0200

    Remove reference to old k8s_pod_name external id

--- ovn_k8s/lib/ovn.py
+++ ovn_k8s/lib/ovn.py
@@ -74,7 +74,7 @@ def create_ovn_acl(ls_name, pod_name, lport_name, priority, match, action):
     # an external id for the ACL as well (acl-add won't return the ACL id)
     command = ('-- --id=@acl_id create ACL action=%s direction=to-lport '
                'priority=%d match="%s" external_ids:lport_name=%s '
-               'external_ids:k8s_pod_name=%s -- '
+               'external_ids:pod_name=%s -- '
                'add Logical_Switch %s acls @acl_id' %
                (action, priority, match, lport_name, pod_name, ls_name))
     command_items = tuple(shlex.split(command))
