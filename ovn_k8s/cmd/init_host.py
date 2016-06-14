907d3f3589724fc25a7ce99c37a08939e56e479d
Salvatore <salv.orlando@gmail.com>
2016-06-14 08:24:33 +0200

    init_host: make arguments required and positional
    As one cannot really make up the host name and the host cidr
    arguments, this patch ensures they are required by the CLI
    command.
    
    Also, the arguments are made positional rather than keyword
    as it'll take less time to type the command in this way.

--- ovn_k8s/cmd/init_host.py
+++ ovn_k8s/cmd/init_host.py
@@ -7,8 +7,8 @@ from oslo_log import log
 from ovn_k8s import config
 
 opts = [
-    cfg.StrOpt('host_name'),
-    cfg.StrOpt('host_subnet')
+    cfg.StrOpt('host_name', required=True, positional=True),
+    cfg.StrOpt('host_subnet', required=True, positional=True)
 ]
 LOGFILE = 'k8s_init_host.log'
 LOG = log.getLogger(__name__)
@@ -17,9 +17,13 @@ LOG = log.getLogger(__name__)
 def main():
     log.register_options(cfg.CONF)
     cfg.CONF.register_cli_opts(opts)
-    config.init_conf(sys.argv[1:])
-    cfg.CONF.set_override('log_file', LOGFILE)
-    cfg.CONF.set_override('debug', True)
-    log.setup(cfg.CONF, 'k8s_init_host')
-    cfg.CONF.log_opt_values(LOG, logging.DEBUG)
-    config.init_host(cfg.CONF.host_name, cfg.CONF.host_subnet)
+    try:
+        config.init_conf(sys.argv[1:])
+    except cfg.RequiredOptError:
+        cfg.CONF.print_usage()
+    else:
+        cfg.CONF.set_override('log_file', LOGFILE)
+        cfg.CONF.set_override('debug', True)
+        log.setup(cfg.CONF, 'k8s_init_host')
+        cfg.CONF.log_opt_values(LOG, logging.DEBUG)
+        config.init_host(cfg.CONF.host_name, cfg.CONF.host_subnet)
